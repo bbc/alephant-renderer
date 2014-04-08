bf2556696acf6a27469b5a8d1146213bb5d6c7bc
Steven Jack <stevenmajack@gmail.com>
2014-04-08 16:20:23 +0100

    Return a lambda in the translation instead of a standard value. Also adds template name as prefix key for translation

--- lib/alephant/views/base.rb
+++ lib/alephant/views/base.rb
@@ -1,16 +1,16 @@
 require 'mustache'
 require 'alephant/views'
 require 'hashie'
-
+require 'json'
 require 'i18n'
 
 module Alephant::Views
   class Base < Mustache
     attr_accessor :data, :locale
 
-    def initialize(data = {})
+    def initialize(data = {}, locale = 'en')
       @data = Hashie::Mash.new data
-      @locale = 'en'
+      @locale = locale
     end
 
     def locale=(l)
@@ -19,7 +19,14 @@ module Alephant::Views
 
     def t(*args)
       I18n.locale = @locale
-      I18n.t(*args)
+      lambda do |comma_delimited_args|
+        args   = comma_delimited_args.strip.split ','
+        key    = args.shift
+        params = args.empty? ? {} : JSON.parse(args.first).with_indifferent_access
+        prefix = /\/([^\/]+)\./.match(template_file)[1]
+
+        I18n.translate("#{prefix}.#{key}", params)
+      end
     end
 
     def self.inherited(subclass)
