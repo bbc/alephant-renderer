e3e05febe55cc65ddfbaff20dfff6704c5112334
Dan Arnould <dan@arnould.co.uk>
2014-10-07 18:27:00 +0100

    Add missing require to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,5 @@ require 'alephant/renderer'
 require 'alephant/renderer/views'
 require 'alephant/renderer/views/json'
 require 'alephant/renderer/views/html'
+require 'alephant/renderer/view_mapper'
 
