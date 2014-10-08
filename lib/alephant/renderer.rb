92274de86923bf90ea4a6b3cc614b9833e2a735b
Charlie Revett <charlierevett@gmail.com>
2014-10-08 17:51:27 +0100

    Renderer spec

--- lib/alephant/renderer.rb
+++ lib/alephant/renderer.rb
@@ -1,19 +1,18 @@
 require 'alephant/renderer/version'
 require 'alephant/renderer/view_mapper'
-require 'alephant/support/parser'
 
 module Alephant
   module Renderer
-    def self.create(config, message)
-      Renderer.new(config, message)
+    def self.create(config, data)
+      Renderer.new(config, data)
     end
 
     class Renderer
-      attr_reader :config, :message
+      attr_reader :config, :data
 
-      def initialize(config, message)
-        @config  = config
-        @message = message
+      def initialize(config, data)
+        @config = config
+        @data   = data
       end
 
       def views
@@ -22,20 +21,10 @@ module Alephant
 
       private
 
-      def data
-        @data ||= parser.parse message
-      end
-
       def mapper
         @mapper ||= ViewMapper.new(
           config[:renderer_id],
           config[:view_path]
-        )
-      end
-
-      def parser
-        @parser ||= Support::Parser.new(
-          config[:msg_vary_id_path]
         )
       end
     end
---
+++ spec/renderer_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe Alephant::Renderer do
+  subject { Alephant::Renderer }
+
+  let(:config) { 
+    {
+      renderer_id: 'foo',
+      view_path: File.join(File.dirname(__FILE__), 'fixtures/components'),
+    }
+  }
+  let(:data) { {content: 'test'} } 
+
+  describe '.create' do
+    context 'using valid params' do
+      let(:expected) { Alephant::Renderer::Renderer }
+
+      specify { 
+        expect(subject.create(config, data)).to be_instance_of expected
+      } 
+    end
+  end
+
+  describe Alephant::Renderer::Renderer do
+    subject { Alephant::Renderer.create(config, data) }
+
+    describe '#config' do
+      specify { expect(subject.config).to eql config }
+    end
+
+    describe '#data' do
+      specify { expect(subject.data).to eql data }
+    end
+
+    describe '#views' do
+      it 'returns a Hash' do
+        expect(subject.views).to be_a Hash
+      end
+
+      context 'using three Models' do
+        it 'returns three Views in Hash' do
+          expect(subject.views.length).to eql 3
+        end
+      end
+
+      context 'using `bar`, `foo`, `json` models' do
+        it 'contains a View for `bar` model' do
+          expect(subject.views.key? 'bar').to be
+        end
+
+        it 'contains a View for `foo` model' do
+          expect(subject.views.key? 'foo').to be 
+        end
+
+        it 'contains a View for `json` model' do
+          expect(subject.views.key? 'json').to be
+        end
+      end
+    end
+  end
+end
